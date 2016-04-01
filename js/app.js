8364c809738dfef4742f7c0394d0623eedffaab5
Marcelo Contreras <marcelojoc@hotmail.com>
2016-04-01 18:18:57 -0300

    retoques de plantilla y agregado de imagenes + implementacion de Spinner

--- js/app.js
+++ js/app.js
@@ -26,22 +26,47 @@ function getJson(url, fRetorno){
 
 
 
-// (function()
-// { 
+var opts = {
+  lines: 11 // The number of lines to draw
+, length: 19 // The length of each line
+, width: 21 // The line thickness
+, radius: 41 // The radius of the inner circle
+, scale: 0.25 // Scales overall size of the spinner
+, corners: 1 // Corner roundness (0..1)
+, color: '#000' // #rgb or #rrggbb or array of colors
+, opacity: 0.25 // Opacity of the lines
+, rotate: 0 // The rotation offset
+, direction: 1 // 1: clockwise, -1: counterclockwise
+, speed: 1 // Rounds per second
+, trail: 60 // Afterglow percentage
+, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+, zIndex: 2e9 // The z-index (defaults to 2000000000)
+, className: 'spinner' // The CSS class to assign to the spinner
+, top: '50%' // Top position relative to parent
+, left: '50%' // Left position relative to parent
+, shadow: false // Whether to render a shadow
+, hwaccel: false // Whether to use hardware acceleration
+, position: 'absolute' // Element positioning
+}
+var target = document.getElementById('contenido');
+var spinner = new Spinner(opts).spin(target);
 
-//     window.addEventListener('load', function(){
+(function()
+{ 
 
-//         getJson("datos.json", function(dato){
+    window.addEventListener('load', function(){
 
-//         var contenedor = document.getElementById("contenido");
-//         var tmpl = document.getElementById("articulo_tpl").innerHTML;  
-//         var plantilla = Handlebars.compile(tmpl); 
-//         var html= plantilla(dato);
-//         contenedor.innerHTML=html;
-//         });
-//     });
+        getJson("datos.json", function(dato){
 
-// })();
+        var contenedor = document.getElementById("contenido");
+        var tmpl = document.getElementById("articulo_tpl").innerHTML;  
+        var plantilla = Handlebars.compile(tmpl); 
+        var html= plantilla(dato);
+        contenedor.innerHTML=html;
+        });
+    });
+
+})();
 
 
 
